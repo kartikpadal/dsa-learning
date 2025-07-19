5b147508bb11c45328b0394a205311f731cefac5
kartikpadal <kartikpadal17@gmail.com>
2025-07-19 17:06:17 +0530

    learned this important stuff, which i didn't know

---
+++ PrettyPrinting.java
@@ -0,0 +1,13 @@
+
+
+public class PrettyPrinting {
+    public static void main(String[] args) {
+        float a = 453.1274f;
+        System.out.printf("Formatted number is %.2f", a);
+        // prints 453.13
+        System.out.printf("Pie: %.2f", Math.PI);
+        // prints 3.14
+        System.out.printf("Hello my name is %s and I am %s", "Kartik", "21");
+        // prints Hello my name is Kartik and I am 21
+    }
+}
