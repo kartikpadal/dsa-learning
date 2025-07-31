dc9111228dd21fe789901d647a6f2b109099b73a
kartikpadal <kartikpadal17@gmail.com>
2025-07-31 17:33:56 +0530

    josh prolem 1

--- BottleAndCaps.java
+++ BottleAndCaps.java
@@ -1,3 +1,35 @@
-public class BottleAndCaps {
+import java.util.Scanner;
 
+public class BottleAndCaps {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s  = sc.nextLine();
+        int c = 0;
+        String[] arr = s.split(",");
+
+        int n = arr.length;
+        boolean[] used = new boolean[n];
+        for(int i=0; i<n; i++){
+            String item = arr[i];
+            if(used[i]) continue;
+            String num = item.substring(0,item.length()-1);
+            char type  = item.charAt(item.length()-1);
+            String match = "";
+            if(type == 'B'){
+                match = num + "C";
+            }
+            else{
+                match = num + "B";
+            }
+            for(int j=i+1; j <= i+3 && j<n; j++){
+                if(arr[j].equals(match)){
+                    used[i] = true;
+                    used[j] = true;
+                    c++;
+                    break;
+                }
+            }
+        }
+        System.out.println(c);
+    }
 }
