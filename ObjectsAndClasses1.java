9a04d7822d713a07fe6202565544e03219846002
kartikpadal <kartikpadal17@gmail.com>
2025-08-01 23:03:29 +0530

    experimenting about objects and constructors, constructor overloading

--- ObjectsAndClasses1.java
+++ ObjectsAndClasses1.java
@@ -6,19 +6,20 @@ public class ObjectsAndClasses1 {
 //        s1.marks = 45.67f;
 
         Student s1 = new Student(15,"kartik",56.78f);
-
-
-        Student s2 = new Student();
-        System.out.println(s2.rno);
-        System.out.println(s2.name);
-        System.out.println(s2.marks);
-
         s1.greeting();
         System.out.println(s1.name);
         s1.changeName("padal");
         System.out.println(s1.rno);
         System.out.println(s1.name);
         System.out.println(s1.marks);
+
+        //calls the empty Student()
+        Student s2 = new Student();
+        System.out.println(s2.rno);
+        System.out.println(s2.name);
+        System.out.println(s2.marks);
+
+
 
     }
 }
@@ -28,6 +29,7 @@ class Student {
     String name;
     float marks;
 
+    // gets called only when i pass 3 args from any Student()
     Student(int rno, String name, float marks){
         this.rno = rno;
         this.name = name;
@@ -42,15 +44,17 @@ class Student {
         this.name = naam;
     }
 
+
     Student  (Student other) {
         this.name = other.name;
         this.rno = other.rno;
         this.marks = other.marks;
     }
 
+    // gets called when i dont pass any args
     Student () {
         // this is how you call a constructor from another constructor
         // internally: new Student (13, "default person", 100.0f);
-        this (13, "default person", 100.0f);
+        this (1, "default person", 100.0f);
     }
 }
