8994e7cdcf196b24ce1baa9a8bffa2c23f322d49
kartikpadal <kartikpadal17@gmail.com>
2025-08-02 10:47:46 +0530

    learning about 'static' and static variables

--- com/oop/staticExample/Main.java
+++ com/oop/staticExample/Main.java
@@ -2,11 +2,9 @@ package com.oop.staticExample;
 
 public class Main {
     public static void main(String[] args) {
-        Human kunal = new Human(22, "Kunal", 10000, false);
-        Human rahul = new Human(34, "Rahul", 15000, true);
-        Human arpit = new Human(34, "arpit", 15000, true);
+        Human kartik = new Human(21, "Kartik", 10000, false);
 
-        System.out.println(Human.population);
-
+        System.out.println(Human.population);  // 1
+        System.out.println(kartik.name);
     }
 }
