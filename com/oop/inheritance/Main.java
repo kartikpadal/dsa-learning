5caabacc0eaa6f8d19df9d786ccc6a593389bd9f
kartikpadal <kartikpadal17@gmail.com>
2025-08-05 15:36:46 +0530

    learned upcasting in inheritance

--- com/oop/inheritance/Main.java
+++ com/oop/inheritance/Main.java
@@ -2,9 +2,14 @@ package com.oop.inheritance;
 
 public class Main {
     public static void main(String[] args) {
-        Box a = new Box();
-        BoxWeight b = new BoxWeight(3,4,5,10);
+        Box a = new Box(3,4,5);
         System.out.println(a.h);
-        System.out.println(b.h+", "+b.l+", "+b.w+", "+b.weight);
+
+        Box b = new BoxWeight(3,4,5,9); // This is upcasting: assigning a subclass object to a superclass reference
+        System.out.println(b.h+", "+b.l+", "+b.w); // allowed
+        // System.out.println(b.weight); ❌ error
+
+
+
     }
 }
