b80d33f548dafa785ac6e84bec4d43d21e7f021c
kartikpadal <kartikpadal17@gmail.com>
2025-08-05 15:02:38 +0530

    learned 'super' keyword

--- com/oop/inheritance/Main.java
+++ com/oop/inheritance/Main.java
@@ -3,8 +3,8 @@ package com.oop.inheritance;
 public class Main {
     public static void main(String[] args) {
         Box a = new Box();
-        Box b = new Box(3,4,5);
+        BoxWeight b = new BoxWeight(3,4,5,10);
         System.out.println(a.h);
-        System.out.println(b.h+""+b.l+""+b.w);
+        System.out.println(b.h+", "+b.l+", "+b.w+", "+b.weight);
     }
 }
