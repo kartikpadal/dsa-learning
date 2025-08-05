1cf382071c7f32cc4e9b4ab3d70b1ad7a11b0989
kartikpadal <kartikpadal17@gmail.com>
2025-08-05 15:49:11 +0530

    learned upcasting in inheritance

--- com/oop/inheritance/Main.java
+++ com/oop/inheritance/Main.java
@@ -5,9 +5,13 @@ public class Main {
         Box a = new Box(3,4,5);
         System.out.println(a.h);
 
+        BoxWeight b2 = new BoxWeight(1,2,3,4);        //Full access to all members, including
+        System.out.println(b2.h+", "+b2.l+", "+b2.w+", "+b2.weight);   //inherited ones and those declared in BoxWeight
+
         Box b = new BoxWeight(3,4,5,9); // This is upcasting: assigning a subclass object to a superclass reference
         System.out.println(b.h+", "+b.l+", "+b.w); // allowed
-        // System.out.println(b.weight); ❌ error
+        // System.out.println(b.weight);  error: can only access Box members
+
 
 
 
