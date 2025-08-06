71dd481100b0eb2914b554b44ee394ab7790ee6e
kartikpadal <kartikpadal17@gmail.com>
2025-08-06 21:05:37 +0530

    this ques was asked to me in kanaka campus exam

---
+++ kanakaQues.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class kanakaQues {
+
+        public static int findMinI(int N) {
+            int st = 0;
+            int e = N; // Safe upper bound since S(i) grows fast
+
+            int ans = -1;
+
+            while (st <= e) {
+                int m = st + (e - st) / 2;
+                int sum = (m % 2 == 0) ? (m * m + m + 2) : (m * m + 3);
+
+                if (sum >= N) {
+                    ans = m;
+                    e = m - 1; // try to find smaller i
+                } else {
+                    st = m + 1;
+                }
+            }
+
+            return ans;
+        }
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int N = sc.nextInt();
+            System.out.println(findMinI(N));  // Example input
+        }
+    }
+
+
