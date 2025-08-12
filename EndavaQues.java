c4bebd89b184eeb42ac98e3e8c09c7bd4fc9b840
kartikpadal <kartikpadal17@gmail.com>
2025-08-12 22:11:49 +0530

    this was my question for endava test

---
+++ EndavaQues.java
@@ -0,0 +1,21 @@
+public class EndavaQues {
+    public static void main(String[] args) {
+        System.out.println(funct("1234567", 3));
+    }
+
+    static int funct(String s, int k){
+
+        while(s.length() > 1){
+            int sum=0;
+            for(int i=0; i<s.length(); i++){
+                if(i % k == 0){
+                    int d = s.charAt(i) - '0';
+                    sum += d;
+                }
+            }
+            s = String.valueOf(sum);
+        }
+        return Integer.parseInt(s);
+    }
+
+}
