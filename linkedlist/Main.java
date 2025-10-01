9172bd9f70cea2f213f976fbf5b718d63ffc8811
kartikpadal <kartikpadal17@gmail.com>
2025-10-01 16:18:40 +0530

    implemented singly linked list display feat

--- linkedlist/Main.java
+++ linkedlist/Main.java
@@ -4,6 +4,12 @@ public class Main {
     public static void main(String[] args){
         LL list = new LL();
 
+        list.insertFirst(3);
+        list.insertFirst(4);
+        list.insertFirst(5);
+        list.insertFirst(6);
+
+        list.display();
     }
 }
 
