5a5caf1ace600cd814d9df31d710053f8d7c688f
kartikpadal <kartikpadal17@gmail.com>
2025-11-30 18:21:54 +0530

    getting better now

--- Strings/Stream.java
+++ Strings/Stream.java
@@ -4,24 +4,57 @@ package Strings;
 
 public class Stream {
     public static void main(String[] args){
-        skip("","abbccad");
+        System.out.println(skipBatman("abbatmanccad"));
     }
 
-    static void skip(String p, String up){
+
+    static String skipBatman(String up){
         if(up.isEmpty()){
-            System.out.println(p);
-            return;
+            return "";
+        }
+
+        if(up.startsWith("batman")){
+            return skipBatman(up.substring(6));
+        }
+        else{
+            return up.charAt(0) + skipBatman(up.substring(1));
+        }
+    }
+
+
+    //with recursion
+    static String skip(String up){
+        if(up.isEmpty()){
+            return "";
         }
 
         char ch = up.charAt(0);
 
         if(ch == 'a'){
-            skip(p,up.substring(1));
+            return skip(up.substring(1));
         }
         else{
-            skip(p+ch, up.substring(1));
+            return ch + skip(up.substring(1));
         }
     }
+
+
+    // without recursion
+//    static void skip(String p, String up){
+//        if(up.isEmpty()){
+//            System.out.println(p);
+//            return;
+//        }
+//
+//        char ch = up.charAt(0);
+//
+//        if(ch == 'a'){
+//            skip(p,up.substring(1));
+//        }
+//        else{
+//            skip(p+ch, up.substring(1));
+//        }
+//    }
 
 }
 
