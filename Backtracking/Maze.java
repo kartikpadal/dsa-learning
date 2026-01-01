83a3fc584d1d23f971106ecd1da23d250e26337f
kartikpadal <kartikpadal17@gmail.com>
2026-01-01 21:46:49 +0530

    maze problem (printing paths in an AL) using recursion

--- Backtracking/Maze.java
+++ Backtracking/Maze.java
@@ -1,10 +1,13 @@
 package Backtracking;
 
+import java.util.ArrayList;
+
 public class Maze {
     public static void main(String[] args){
 
         //System.out.println(count(3,3));
-        path("",3,3);
+        //path("",3,3);
+        System.out.println(pathList("",3,3));
     }
 
 
@@ -32,6 +35,25 @@ public class Maze {
         if(c>1){
             path(p+"R", r,c-1);
         }
+    }
+
+    static ArrayList<String> pathList(String p, int r, int c){
+        if(r==1 && c==1){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        ArrayList<String> ans = new ArrayList<>();
+
+        if(r>1){
+            ans.addAll(pathList(p+'D', r-1, c));
+        }
+        if(c>1){
+            ans.addAll(pathList(p+'R', r, c-1));
+        }
+
+        return ans;
     }
 
 }
