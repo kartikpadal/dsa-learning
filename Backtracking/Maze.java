e121d212dd3a23cac08bd7d01c0bea26f851d5a0
kartikpadal <kartikpadal17@gmail.com>
2026-01-01 19:55:10 +0530

    maze problem (printing paths) using recursion

--- Backtracking/Maze.java
+++ Backtracking/Maze.java
@@ -3,7 +3,8 @@ package Backtracking;
 public class Maze {
     public static void main(String[] args){
 
-        System.out.println(count(3,3));
+        //System.out.println(count(3,3));
+        path("",3,3);
     }
 
 
@@ -16,4 +17,21 @@ public class Maze {
 
         return left + right;
     }
+
+
+    // Print the paths (top left to bottom right: can only go right & down)
+    static void path(String p, int r, int c){
+        if(r==1 && c==1){
+            System.out.println(p);
+            return;
+        }
+
+        if(r>1){
+            path(p+"D",r-1, c);
+        }
+        if(c>1){
+            path(p+"R", r,c-1);
+        }
+    }
+
 }
