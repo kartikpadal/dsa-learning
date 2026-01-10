aaee023142d7c3c48594b856c0bdf0a372fec913
kartikpadal <kartikpadal17@gmail.com>
2026-01-10 20:26:21 +0530

    maze with blocks problem (printing paths) using recursion

--- Backtracking/Maze.java
+++ Backtracking/Maze.java
@@ -10,80 +10,107 @@ public class Maze {
         //System.out.println(count(3,3));
         //path("",3,3);
         //System.out.println(pathList("",3,3));
-        System.out.println(count(3,3));
+
+        boolean[][] board = {
+                {true, true, true},
+                {true, false, true},
+                {true, true, true}
+        };
+
+        mazeWithBlocks("",board,0,0);
     }
 
+//
+//    //count no of ways to go from top left to bottom left: can only go right & down
+//    static int count(int r, int c){
+//        if(r == 1 || c == 1) return 1;
+//
+//        int left = count(r-1,c);
+//        int right = count(r, c-1);
+//
+//        return left + right;
+//    }
+//
+//
+//    // Print the paths (top left to bottom right: can only go right & down)
+//    static void path(String p, int r, int c){
+//        if(r==1 && c==1){
+//            System.out.println(p);
+//            return;
+//        }
+//
+//        if(r>1){
+//            path(p+"D",r-1, c);
+//        }
+//        if(c>1){
+//            path(p+"R", r,c-1);
+//        }
+//    }
+//
+//
+//    // Print the paths in an ArrayList (top left to bottom right: can only go right & down)
+//    static ArrayList<String> pathList(String p, int r, int c){
+//        if(r==1 && c==1){
+//            ArrayList<String> list = new ArrayList<>();
+//            list.add(p);
+//            return list;
+//        }
+//
+//        ArrayList<String> ans = new ArrayList<>();
+//
+//        if(r>1){
+//            ans.addAll(pathList(p+'D', r-1, c));
+//        }
+//        if(c>1){
+//            ans.addAll(pathList(p+'R', r, c-1));
+//        }
+//
+//        return ans;
+//    }
+//
+//
+//    // here diagonal is also allowed
+//    static ArrayList<String> pathListDiagonal(String p, int r, int c){
+//        if(r==1 && c==1){
+//            ArrayList<String> list = new ArrayList<>();
+//            list.add(p);
+//            return list;
+//        }
+//
+//        ArrayList<String> ans = new ArrayList<>();
+//
+//        if(r>1 && c>1){
+//            ans.addAll(pathListDiagonal(p+"DG", r-1, c-1));
+//        }
+//
+//        if(r>1){
+//            ans.addAll(pathListDiagonal(p+'D', r-1, c));
+//        }
+//        if(c>1){
+//            ans.addAll(pathListDiagonal(p+'R', r, c-1));
+//        }
+//
+//        return ans;
+//    }
 
-    //count no of ways to go from top left to bottom left: can only go right & down
-    static int count(int r, int c){
-        if(r == 1 || c == 1) return 1;
-
-        int left = count(r-1,c);
-        int right = count(r, c-1);
-
-        return left + right;
-    }
 
 
-    // Print the paths (top left to bottom right: can only go right & down)
-    static void path(String p, int r, int c){
-        if(r==1 && c==1){
+    // maze with blocks:
+    static void mazeWithBlocks(String p, boolean[][] maze, int r, int c){
+        if(r == maze.length - 1 && c == maze[0].length - 1){
             System.out.println(p);
             return;
         }
 
-        if(r>1){
-            path(p+"D",r-1, c);
-        }
-        if(c>1){
-            path(p+"R", r,c-1);
-        }
-    }
+        if(!maze[r][c]) return;
 
-
-    // Print the paths in an ArrayList (top left to bottom right: can only go right & down)
-    static ArrayList<String> pathList(String p, int r, int c){
-        if(r==1 && c==1){
-            ArrayList<String> list = new ArrayList<>();
-            list.add(p);
-            return list;
+        if(r < maze.length - 1){
+            mazeWithBlocks(p + 'D', maze, r+1, c);
         }
 
-        ArrayList<String> ans = new ArrayList<>();
-
-        if(r>1){
-            ans.addAll(pathList(p+'D', r-1, c));
+        if(c < maze[0].length - 1){
+            mazeWithBlocks(p + 'R', maze, r, c+1);
         }
-        if(c>1){
-            ans.addAll(pathList(p+'R', r, c-1));
-        }
-
-        return ans;
-    }
-
-
-    // here diagonal is also allowed
-    static ArrayList<String> pathListDiagonal(String p, int r, int c){
-        if(r==1 && c==1){
-            ArrayList<String> list = new ArrayList<>();
-            list.add(p);
-            return list;
-        }
-
-        ArrayList<String> ans = new ArrayList<>();
-
-        if(r>1 && c>1){
-            ans.addAll(pathListDiagonal(p+"DG", r-1, c-1));
-        }
-
-        if(r>1){
-            ans.addAll(pathListDiagonal(p+'D', r-1, c));
-        }
-        if(c>1){
-            ans.addAll(pathListDiagonal(p+'R', r, c-1));
-        }
-
-        return ans;
     }
 
 }
