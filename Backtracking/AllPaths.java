2aa342fc3a79edad201bfbfdc9ddf25db1ad32a5
kartikpadal <kartikpadal17@gmail.com>
2026-01-16 23:43:27 +0530

    maze with all possible paths problem (printing paths) using recursion

--- Backtracking/AllPaths.java
+++ Backtracking/AllPaths.java
@@ -7,35 +7,39 @@ public class AllPaths {
                 {true, true, true},
                 {true, true, true}
         };
+
+        allPath("", board, 0,0);
     }
 
 
 
-    static void allPath(String p, boolean [][] maze, int r, int c){
-        if(r == maze.length-1 && c == maze[0].length){
+    static void allPath(String p, boolean[][] maze, int r, int c) {
+        if (r == maze.length - 1 && c == maze[0].length - 1) {
             System.out.println(p);
             return;
         }
 
-        if(!maze[r][c]) return;
+        if (!maze[r][c]) {
+            return;
+        }
 
-        // we're considering this block in our path:
+        // we're considering this block in our path
         maze[r][c] = false;
 
-        if(r < maze.length-1){
-            allPath(p+'D', maze, r+1, c);
+        if (r < maze.length - 1) {
+            allPath(p + 'D', maze, r+1, c);
         }
 
-        if(c < maze[0].length - 1){
-            allPath(p+'R', maze, r, c+1);
+        if (c < maze[0].length - 1) {
+            allPath(p + 'R', maze, r, c+1);
         }
 
-        if(r > 0){
-            allPath(p+'U', maze, r-1, c);
+        if (r > 0) {
+            allPath(p + 'U', maze, r-1, c);
         }
 
-        if(c > 0){
-            allPath(p+'L', maze, r, c-1);
+        if (c > 0) {
+            allPath(p + 'L', maze, r, c-1);
         }
 
         // this line is where the function will be over
