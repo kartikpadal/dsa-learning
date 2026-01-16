2a5c68ae2602b7e14a881732e4bd6b78c9958bba
kartikpadal <kartikpadal17@gmail.com>
2026-01-16 23:39:27 +0530

    maze with all possible paths problem (printing paths) using recursion

--- Backtracking/AllPaths.java
+++ Backtracking/AllPaths.java
@@ -9,6 +9,8 @@ public class AllPaths {
         };
     }
 
+
+
     static void allPath(String p, boolean [][] maze, int r, int c){
         if(r == maze.length-1 && c == maze[0].length){
             System.out.println(p);
@@ -20,7 +22,25 @@ public class AllPaths {
         // we're considering this block in our path:
         maze[r][c] = false;
 
-        if()
+        if(r < maze.length-1){
+            allPath(p+'D', maze, r+1, c);
+        }
+
+        if(c < maze[0].length - 1){
+            allPath(p+'R', maze, r, c+1);
+        }
+
+        if(r > 0){
+            allPath(p+'U', maze, r-1, c);
+        }
+
+        if(c > 0){
+            allPath(p+'L', maze, r, c-1);
+        }
+
+        // this line is where the function will be over
+        // so before the function gets removed, also remove the changes that were made by that function
+        maze[r][c] = true;
     }
 
 }
