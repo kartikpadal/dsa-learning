396229498b76ce2aa6f9adce486f97dac7f2b0b5
kartikpadal <kartikpadal17@gmail.com>
2026-01-18 23:00:29 +0530

    maze with all possible paths problem (printing paths & steps in the maze) using recursion

--- Backtracking/AllPaths.java
+++ Backtracking/AllPaths.java
@@ -47,4 +47,36 @@ public class AllPaths {
         maze[r][c] = true;
     }
 
+
+    // printing how the steps look in the matrix:
+
+    static void allPathPrint(String p, boolean [][] maze, int r, int c, int[][] path, int step){
+
+        if(!maze[r][c]){
+            return;
+        }
+
+        maze[r][c] = false;
+        path[r][c] = step;
+
+        if(r < maze.length - 1){
+            allPathPrint(p + 'D', maze, r, c+1, path, step+1);
+        }
+
+        if(c < maze[0].length - 1){
+            allPathPrint(p + 'R', maze, r, c+1, path, step+1);
+        }
+
+        if(r > 0){
+            allPathPrint(p + 'U', maze, r-1, c, path, step+1);
+        }
+
+        if(c > 0){
+            allPathPrint(p + 'L', maze, r, c-1, path, step+1);
+        }
+
+        maze[r][c] = true;
+        path[r][c] = 0;
+    }
+
 }
