8777616cbfaa8b6333f98dfed6f399823811443f
kartikpadal <kartikpadal17@gmail.com>
2026-01-25 22:10:38 +0530

    Capgemini Q2

---
+++ CapgeminiQ2.java
@@ -0,0 +1,46 @@
+
+import java.util.*;
+
+
+
+public class CapgeminiQ2 {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            if (n == 0) {
+                System.out.println(-1);
+                return;
+            }
+
+            // Step 1: Build rightMax array
+            int[] rightMax = new int[n];
+            rightMax[n - 1] = arr[n - 1];
+
+            for (int i = n - 2; i >= 0; i--) {
+                rightMax[i] = Math.max(arr[i], rightMax[i + 1]);
+            }
+
+            // Step 2: Two-pointer technique
+            int i = 0, j = 0, maxDiff = -1;
+
+            while (i < n && j < n) {
+                if (arr[i] <= rightMax[j]) {
+                    maxDiff = Math.max(maxDiff, j - i);
+                    j++;
+                } else {
+                    i++;
+                }
+            }
+
+            System.out.println(maxDiff);
+        }
+    }
+
+
