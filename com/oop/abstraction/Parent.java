a3f7592e1a12ba1a1d8ec25a2d3ff66bafec4b36
kartikpadal <kartikpadal17@gmail.com>
2026-01-30 17:58:12 +0530

    Abstraction

--- com/oop/abstraction/Parent.java
+++ com/oop/abstraction/Parent.java
@@ -18,4 +18,17 @@ public abstract class Parent {
         System.out.println("*Chirps*");
     }
 
+    // cant create abstract static methods in an abstract class since abstract methods need to get overridden
+    // and static methods cant get overridden but u can create normal static methods tho:
+    static void hello(){
+        System.out.println("hi");
+    }
+
+    // can also have normal methods:
+    void normal(){
+        System.out.println("a normal method");
+    }
+
+
+
 }
